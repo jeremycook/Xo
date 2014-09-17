22e0679e11a88e7d20afeebd8fb47cd755622b17
Jeremy Cook <jeremycook.co@gmail.com>
2014-09-16 21:51:38 -0600

    Added default MVC solution, project, test project

---
+++ Xo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Xo.Startup))]
+namespace Xo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
