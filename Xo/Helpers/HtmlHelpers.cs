48f3edc937339e4720824e202e1b08d31208aaf1
Jeremy Cook <jeremycook.co@gmail.com>
2014-09-29 18:10:55 -0600

    Added bootstrap label HTML helpers

---
+++ Xo/Helpers/HtmlHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Xo.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static IHtmlString BootstrapLabelFor<TModel, TProp>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TProp>> expression,
+            string columnClass = "col-md-2")
+        {
+            return html.LabelFor(expression, htmlAttributes: new { @class = columnClass + " control-label" });
+        }
+
+        public static IHtmlString BootstrapLabel(
+            this HtmlHelper html,
+            string propertyName,
+            string columnClass = "col-md-2")
+        {
+            return html.Label(propertyName, htmlAttributes: new { @class = columnClass + " control-label" });
+        }
+    }
+}
