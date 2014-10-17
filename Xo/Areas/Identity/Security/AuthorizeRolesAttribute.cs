9f67a13708d44c492154b0612c7ce19568839d21
Jeremy Cook <jeremycook.co@gmail.com>
2014-10-17 06:25:46 -0600

    Added authorize roles that takes Role IDs

---
+++ Xo/Areas/Identity/Security/AuthorizeRolesAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Xo.Areas.Identity.Models;
+
+namespace Xo.Areas.Identity.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        public AuthorizeRolesAttribute(params RoleId[] roleIds)
+        {
+            Roles = string.Join(",", roleIds.Select(o => o.ToString()));
+        }
+    }
+}
